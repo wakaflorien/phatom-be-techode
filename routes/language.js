5b5fa3b81e04aee95ce3bf51b14728b64dac98f3
FLORIEN NIYONGABO <48131185+wakaflorien@users.noreply.github.com>
2022-05-12 15:43:43 +0200

    ft(bugs) adding assigned routes to buses all list

--- routes/language.js
+++ routes/language.js
@@ -1,8 +1,13 @@
 import express from 'express'
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 const router = express.Router()
 
 router.get('/welcome', (req,res) => {
     res.status(200).json({ "status": `${req.t('success')}`, "data": `${req.t('welcome')}`})
 })
 
-export default router
+export default router;
+
